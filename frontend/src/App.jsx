64a47ef2940bc457b75d83cd228f6bbf731119b1
Samir Alam <samiramrullah@gmail.com>
2024-04-27 18:44:03 +0530

    dashbaord added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import { Routes,Route } from 'react-router-dom'
+import Layout from './pages/Dashbaord/Layout'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Hero/>}/>
+        <Route path='/dashbaord' element={<Layout/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
