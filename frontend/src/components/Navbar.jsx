8ad408a9a05d7884887f7e9df5d7f7a4e4e860b5
Samir Alam <samiramrullah@gmail.com>
2024-07-14 23:36:52 +0530

    navbar-responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuToggle = () => {
+      setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <nav class="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -15,9 +19,9 @@ const Navbar = () => {
             class="h-8"
             alt="Income-Expenses-Tracker"
           />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap ">
+          {/* <span class="self-center text-2xl font-semibold whitespace-nowrap ">
             Tracker
-          </span>
+          </span> */}
         </Link>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link to={'/signup'}>
@@ -34,6 +38,7 @@ const Navbar = () => {
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             aria-controls="navbar-sticky"
             aria-expanded="false"
+            onClick={handleMenuToggle}
           >
             <span class="sr-only">Open main menu</span>
             <svg
@@ -53,16 +58,13 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
-        <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+        <div className={`${isMenuOpen ? 'flex backdrop-blur-md text-base hover:text-blue-600  ' : 'hidden'} flex-col items-center justify-between w-full md:flex md:w-auto md:order-1`} >
+          
+          <ul class="w-full flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
             <li>
               <Link
                 to={'/'}
-                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
-                aria-current="page"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
               >
                 Home
               </Link>
@@ -70,7 +72,7 @@ const Navbar = () => {
             <li>
               <Link
                 to={'/about'}
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
               >
                 About
               </Link>
@@ -79,7 +81,7 @@ const Navbar = () => {
             <li>
               <Link
                 to={'/contact'}
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
               >
                 Contact
               </Link>
--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 mt-2">
+    <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4 mt-16 md:mt-2">
       <div className="flex flex-col lg:flex-row justify-between gap-8">
         <div className="w-full lg:w-5/12 flex flex-col justify-center">
           <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">About Me</h1>
